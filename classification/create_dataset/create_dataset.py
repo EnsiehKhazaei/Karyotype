f433ec77de8d3168939d2e26de2c338135e1a1de
Ensieh Khazaei <54174012+EnsiehKhazaei@users.noreply.github.com>
2022-04-29 23:11:04 +0430

    Update and rename preprocess_imgs.py to create_dataset.py

--- classification/create_dataset/create_dataset.py
+++ classification/create_dataset/create_dataset.py
@@ -1,8 +1,13 @@
 import cv2
 import os
+import numpy as np
+
+
+def onehot_labels(labels):
+    return np.eye(24)[labels]  
 
 def add_padding(im):
-  old_size = im.shape[:2] # old_size is in (height, width) format
+  old_size = im.shape[:2] 
 
   ## Scale larger images and then padding them if it is necessary
   if (old_size[0]>128 or old_size[1]>32):
@@ -34,49 +39,35 @@ def add_padding(im):
   return new_im
 
 ## Create  dataset
-Data_dir = 'images/'
-train_data = []
-test_data  = []
-validation_data = []
-length_data2   = []
-area_data2     = []
-IMG_SIZE_1, IMG_SIZE_2 = 32, 128
+imgs_dir = 'images/'
+txts_dir = ''
+num_cls = 24
+
 X_train = []
-Xp_train = []
 y_train = []
 X_area_train   = []
 X_length_train = []
 ## Creat train data
-for i in range(len(all_txts_train)):
-    img = all_imgs_train[i]
-    img_array = cv2.imread(os.path.join(Data_dir,img), cv2.IMREAD_GRAYSCALE)
-    new_array = add_padding(img_array)
-    lbl = int(os.path.splitext(img)[0].split('-')[-2].strip())
+for name in os.listdir(imgs_dir):#for i in range(len(all_txts_train)):
+    # img = all_imgs_train[i]
+    img = cv2.imread(os.path.join(imgs_dir, name), cv2.IMREAD_GRAYSCALE)
+    img = np.transpose(add_padding(img))
+    lbl = int(os.path.splitext(name)[0].split('-')[-2].strip())
     
-X_train = np.array(Xp_train).reshape(-1, IMG_SIZE_1, IMG_SIZE_2, 1)
+    text_file = open(os.path.join(txts_dir, name.split('.')[0]+'.txt'),'r')
+    area = float(text_file.readline())
+    length = float(text_file.readline())
+    
+    X_train.append(img)
+    y_train.append(lbl)
+    X_area_train.append(area)
+    X_length_train.append(length)
+    
+X_train = np.array(X_train)
 X_area_train = np.array(X_area_train)
 X_length_train = np.array(X_length_train)
 X_train = X_train/255.0
 y_train = np.array(y_train).reshape(-1,1)
 
-from keras.models import Sequential , Model
-from keras.layers import Dense, Dropout, Flatten , GlobalAveragePooling2D , Input, Conv2D
-from keras.applications.mobilenet import MobileNet
-from keras.applications.resnet50 import ResNet50
-import tensorflow as tf 
-
-## Define onehot_label function
-def onehot_labels(labels):
-    return np.eye(24)[labels]  
-
-num_cls = 24
 y_train = onehot_labels(y_train)
-y_valid = onehot_labels(y_valid)
-y_test  = onehot_labels(y_test)
-print(y_train.shape)
-print(y_test.shape)
 y_train = y_train.reshape((X_train.shape[0], num_cls))
-    text_file = open(all_txts_train[i],'r')
-    area = (float(text_file.readline()))
-    length = (float(text_file.readline()))
-    train_data.append([new_array, area, length, lbl])
